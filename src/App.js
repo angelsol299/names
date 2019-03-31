1efedced5ac9d16c30fc697dda80b44e3fd0d94f
angelsol299 <angelsol299@hotmail.com>
2019-03-31 22:15:49 +0200

    adding components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(20);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Name: <input type="text" />
+          Name: <input type="text" placeholder="enter your name" />
         </p>
-        <p>Age:</p>
+        <p>
+          Age:
+          <input type="text" placeholder="enter your age" />
+        </p>
         <button>Increment Age by 1</button>
+        <br />
+        <button>Decrement Age by 1</button>
       </header>
     </div>
   );
